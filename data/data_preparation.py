f542e20893cdb0ec2c7346f42415479ba21e67d1
mukut03 <ilovecars252003mukut@gmail.com>
2024-04-18 14:45:28 -0500

    moved directory

--- data/data_preparation.py
+++ data/data_preparation.py
@@ -3,12 +3,14 @@ import numpy as np
 import torch
 from torch.utils.data import Dataset
 
+
 def audio_to_mel(path, n_mels=128, n_fft=2048, hop_length=512):
     import librosa
     y, sr = librosa.load(path, sr=None)
     mel = librosa.feature.melspectrogram(y, sr=sr, n_fft=n_fft, hop_length=hop_length, n_mels=n_mels)
     mel_db = librosa.power_to_db(mel, ref=np.max)
     return mel_db
+
 
 class AudioDataset(Dataset):
     def __init__(self, directory, transform=None):
